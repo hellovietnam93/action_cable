4d51522d14c4b3bc79b7391d9dffb3b42b87d9af
TrungNT <hellovietnam93@gmail.com>
2016-07-08 15:03:52 +0700

    Init

---
+++ app/models/message.rb
@@ -0,0 +1,12 @@
+class Message < ApplicationRecord
+  after_create {MessageBroadcastJob.new().perform self}
+
+  belongs_to :user
+  belongs_to :chat_room
+
+  validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+
+  def timestamp
+    created_at.strftime "%H:%M:%S %d %B %Y"
+  end
+end
